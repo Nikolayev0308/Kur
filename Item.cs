d590f5b90d52df7759c9168fc4da31f50fa309c5
Arthur Nikolaev <star03082001@gmail.com>
2021-11-22 20:09:10 +0300

    Улучшенный проект

---
+++ Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kur
+{
+    public abstract class Item
+    {
+        public string name {get; set;}
+        public int count {get; set;}
+        public int price {get; set;}
+        public string model {get; set;}
+    }
+}
